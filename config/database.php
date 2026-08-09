73a26352714f4f51fac7b0e8be13aee6615da7e6
abolfazl <abolfazl2277999@gmail.com>
2026-08-09 14:32:12 +0330

    debug

--- config/database.php
+++ config/database.php
@@ -1,12 +1,10 @@
-<?php
+﻿<?php
 
 // Made by AI (kharej az dars)
 require __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
-
-
 
 $dsn = "mysql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_DATABASE']};charset=utf8mb4";
 
